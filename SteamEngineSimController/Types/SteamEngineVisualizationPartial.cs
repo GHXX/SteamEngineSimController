7bfdc286855f0c4c31e8a940c716c9c640557209
GHXX <GHXX@gmx.at>
2026-01-17 13:01:45 +0100

    add more fields and info

--- SteamEngineSimController/Types/SteamEngineVisualizationPartial.cs
+++ SteamEngineSimController/Types/SteamEngineVisualizationPartial.cs
@@ -4,7 +4,29 @@ namespace SteamEngineSimController.Types;
 
 [StructLayout(LayoutKind.Sequential)]
 public readonly struct SteamEngineVisualizationPartial {
-    // some leading fields are omitted entirely.
+    // some leading fields are probably missing; most fields known thanks to ange
+
+    public readonly IntPtr m_engineBridge;
+    public readonly IntPtr m_simulation;
+    public readonly IntPtr m_simulationBridge;
+
+    public readonly IntPtr m_flame;
+    public readonly IntPtr m_overflowBall, m_overflowArrow;
+    public readonly IntPtr m_pressureNeedle, m_pressureChangeNeedle,
+                           m_waterTemperatureNeedle, m_waterTemperatureChangeNeedle,
+                           steamTemperatureNeedle, m_rpmNeedle,
+                           m_maxPressureNeedle;
+
+    public readonly IntPtr m_waterPump, m_waterFilter,
+                           m_waterPipe, m_waterFilterSensor;
+
+    public readonly IntPtr m_waterDisplay, m_sedimentDisplay;
+    public readonly IntPtr m_heatElement;
+    public readonly IntPtr m_burner;
+    public readonly IntPtr m_throttleValve, m_blowoffValveAdjustment;
+    public readonly IntPtr m_blowOffValve;
+    public readonly IntPtr m_steamWhistlehandle;
+    public readonly IntPtr m_blowOffValveSpringObject;
 
     public readonly IntPtr waterPumpVis;
 
@@ -18,3 +40,5 @@ public readonly struct SteamEngineVisualizationPartial {
 
     public readonly IntPtr pressureReadout, waterTempReadout, waterLevelReadout;
 }
+
+// RadialSlider size: 1296=0x510 bytes (confirmed via cheatengine mem layout, reverser and prv and throttle appear stacked after eachother, like in an array of structs)
